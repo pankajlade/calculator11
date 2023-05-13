63e5bc740132476b9bd955ed708e21583671d761
pankajlade <pankajlade110@gmail.com>
2023-05-13 19:26:53 +0530

    main added again

--- main.cpp
+++ main.cpp
@@ -10,14 +10,13 @@ int main() {
   int b = 5;
 
 
-  std::cout << "okay something bro +++++++++++++" << std::endl;
-  std::cout << "okay something bro +++++++++++++" << std::endl;
-  std::cout << "a __+ b = " << calc.add(a, b) << std::endl;
+  std::cout << "okay PANKAJ bro +++++++++++++" << std::endl;
+  std::cout << "a + b = " << calc.add(a, b) << std::endl;
   std::cout << "a - b = " << calc.subtract(a, b) << std::endl;
   std::cout << "a * b = " << calc.multiply(a, b) << std::endl;
   std::cout << "a / b = " << calc.divide(a, b) << std::endl;
   std::cout << "cooboi69"<<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
+  std::cout << "MY PANKAJ ADDED ..........." <<std::endl;
   
   return 0;
 }
