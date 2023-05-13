05a51eb27d209ac12412bd08f4ae7e956e23595b
pankajlade <pankajlade110@gmail.com>
2023-05-13 13:01:19 +0530

    nasa again hackend

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ int main() {
   int a = 10;
   int b = 5;
 
+
+  std::cout << "okay new bro" << std::endl;
   std::cout << "a + b = " << calc.add(a, b) << std::endl;
   std::cout << "a - b = " << calc.subtract(a, b) << std::endl;
   std::cout << "a * b = " << calc.multiply(a, b) << std::endl;
