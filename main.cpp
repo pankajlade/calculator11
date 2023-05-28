ea4987ffe3bf6dad8d99de60c7594ab357f179a1
pankajlade <pankajlade110@gmail.com>
2023-05-28 12:33:52 +0530

    Removed extra logs.

--- main.cpp
+++ main.cpp
@@ -17,19 +17,6 @@ int main() {
   std::cout << "a / b = " << calc.divide(a, b) << std::endl;
   std::cout << "cooboi69"<<std::endl;
   std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
-  std::cout << "MY good thing.... ADDED ..........." <<std::endl;
   
   return 0;
 }
